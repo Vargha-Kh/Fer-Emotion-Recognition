4b2290d7cf155c7426d1742b924ae25c52a99683
Vargha Khallokhi <vkhalokhi@gmail.com>
2022-10-09 20:34:35 +0330

    Update __init__.py

--- Tensorflow/models/__init__.py
+++ Tensorflow/models/__init__.py
@@ -1,10 +1,12 @@
 from .resnet50 import Resnet50
 from .InceptionResNetV2 import InceptionResnetV2
 from .custom_model import CustomClassifier
+from .regnetx002 import RegNetX002
 
 MODELS = dict(resnet50=Resnet50,
               inception_resnetv2=InceptionResnetV2,
-              custom_model=CustomClassifier
+              custom_model=CustomClassifier,
+              regnetx002=RegNetX002
               # other models
               )
 
