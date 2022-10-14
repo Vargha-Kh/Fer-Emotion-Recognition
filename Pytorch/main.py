766e9165cb60df6c24ccb519411fa22553883b24
Vargha Khallokhi <vkhalokhi@gmail.com>
2022-10-14 19:40:38 +0330

    Update main.py

--- Pytorch/main.py
+++ Pytorch/main.py
@@ -4,11 +4,12 @@ from dataloader import get_dataset
 from model_pytorch import Regnet
 from train import Trainer
 import FERVT
+import math
 
 if __name__ == "__main__":
-    num_classes = 8
+    num_classes = 7
     augmentation = True
-    batch_size = 256
+    batch_size = 128
     num_epochs = 300
     lr = 1e-3
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -23,10 +24,10 @@ if __name__ == "__main__":
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.AdamW(FER_VT.parameters(), lr=0.001)
     # scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.6)
-    scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer,lambda x: ((1 + math.cos(x * math.pi / epochs)) / 2) * (1 - 0.2) + 0.2)
+    scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer,lambda x: ((1 + math.cos(x * math.pi / num_epochs)) / 2) * (1 - 0.2) + 0.2)
     # FER_VT.load_state_dict(torch.load('./model/weight/ck_best.pth'))
     reduce_on_plateau = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', patience=3, verbose=True)
 
     trainer = Trainer(logdir="./logs", csv_log_dir="./csv_log", model_checkpoint_dir="./model")
-    model, optimizer, train_loss, valid_loss = trainer.training(model, train_loader, val_loader, criterion, optimizer,
+    model, optimizer, train_loss, valid_loss = trainer.training(FER_VT, train_loader, val_loader, criterion, optimizer,
                                                                 reduce_on_plateau, device, 50)
