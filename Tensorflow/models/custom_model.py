d3cebb97ebdd8362ef7b2a01bc8473f166c4482e
Vargha Khalokhi <vkhalokhi@gmail.com>
2022-11-12 18:14:16 +0330

    adding keras model

--- Tensorflow/models/custom_model.py
+++ Tensorflow/models/custom_model.py
@@ -16,8 +16,6 @@ class CustomClassifier:
         model = tf.keras.models.Sequential()   
         model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
         model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-        model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.1))
         
         model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
@@ -25,6 +23,8 @@ class CustomClassifier:
         model.add(Conv2D(256, kernel_size=(3, 3), activation='relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Conv2D(512, kernel_size=(3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Conv2D(1024, kernel_size=(3, 3), activation='relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
         
