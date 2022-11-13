15798d0611a8772d6d832bd45e2ac687d38f4dd9
Vargha Khalokhi <vkhalokhi@gmail.com>
2022-11-13 13:04:28 +0330

    plateau fixing

--- Tensorflow/models/custom_model.py
+++ Tensorflow/models/custom_model.py
@@ -8,7 +8,7 @@ from keras import regularizers
 from tensorflow.keras.optimizers import Adam,RMSprop,SGD,Adamax
 
 class CustomClassifier:
-    def __init__(self, img_size=48, channels=3, **kwargs):
+    def __init__(self, img_size=48, channels=1, **kwargs):
         self.input_shape = (img_size, img_size, channels)
 
 
@@ -16,22 +16,17 @@ class CustomClassifier:
         model = tf.keras.models.Sequential()   
         model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
         model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-        model.add(Dropout(0.25))
+        model.add(Dropout(0.1))
         
         model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Conv2D(256, kernel_size=(3, 3), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Conv2D(512, kernel_size=(3, 3), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Conv2D(1024, kernel_size=(3, 3), activation='relu'))
-        model.add(Dropout(0.25))
+        model.add(Dropout(0.2))
         
         model.add(Flatten())
-        model.add(Dense(1024, activation='relu'))
         model.add(Dense(256, activation='relu'))
         model.add(Dense(128, activation='relu'))
-        model.add(Dropout(0.15))
+        model.add(Dropout(0.1))
         model.add(Dense(7, activation='softmax'))
 
         return model
