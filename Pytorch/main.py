5f200c51889eefa3d085fb17b10efc407571e579
Vargha Khalokhi <vkhalokhi@gmail.com>
2022-11-13 16:18:52 +0330

    resnet model with VIT

--- Pytorch/main.py
+++ Pytorch/main.py
@@ -22,15 +22,15 @@ torch.cuda.empty_cache()
 if __name__ == "__main__":
     num_classes = 7
     augmentation = True
-    batch_size = 128
+    batch_size = 256
     num_epochs = 300
 
     # Data Prepare
     train_loader, val_loader = get_dataset()
 
     # Model loading
-    FER_VT = DeepViT(image_size = 48, patch_size = 8, num_classes = num_classes, dim = 1280, depth = 4, heads = 16, mlp_dim = 5120, dropout = 0.1, emb_dropout = 0.1).to(device)
-    #FER_VT = VIT(img_size= (48,48),patch_size= (8,8), emb_dim = 2048, mlp_dim=5120, num_heads=16, num_layers=32, n_classes=num_classes, dropout_rate=0.25, at_d_r=0.1).to(device)
+    #FER_VT = DeepViT(image_size = 48, patch_size = 8, num_classes = num_classes, dim = 1280, depth = 4, heads = 16, mlp_dim = 5120, dropout = 0.1, emb_dropout = 0.1).to(device)
+    FER_VT = VIT(img_size= (48,48),patch_size= (8,8), emb_dim = 1024, mlp_dim=4096, num_heads=16, num_layers=24, n_classes=num_classes, dropout_rate=0.25, at_d_r=0.1).to(device)
     #FER_VT = MyViT((1, 48, 48), n_patches=8, n_blocks=4, hidden_d=1280, n_heads=16, out_d=num_classes).to(device)
     #FER_VT = resnet18_at(num_classes=7, at_type=0).to(device)
 
@@ -51,7 +51,7 @@ if __name__ == "__main__":
     # lr_scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, 0.001, 0.01)
     lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, 200, T_mult=1, eta_min=0.1, last_epoch=- 1, verbose=True)
     #FER_VT.load_state_dict(torch.load('./model/best.pth'))
-    reduce_on_plateau = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', patience=5, verbose=True)
+    reduce_on_plateau = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', patience=10, verbose=True)
     #optimizer = Lookahead( optimizer, alpha= 0.6 , k = 10) 
     #lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=60, gamma=0.2)
     trainer = Trainer(logdir="./logs", csv_log_dir="./csv_log", model_checkpoint_dir="./model")
