488cde1246261e9ed34b8b572cc94f06a7dc9ce5
Vargha Khalokhi <vkhalokhi@gmail.com>
2022-11-17 02:17:42 +0330

    model with VIT

--- Pytorch/main.py
+++ Pytorch/main.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
 
     # Model loading
     # FER_VT = SimpleViT(image_size=img_size, patch_size=8, num_classes=num_classes, dim=1024, depth=6, heads=16, mlp_dim=4096).to(device)
-    FER_VT = VIT(img_size=(3, 48, 48), patch_size=(8, 8), emb_dim=1024, mlp_dim=4096, num_heads=16, num_layers=24,
+    FER_VT = VIT(img_size=(3, 48, 48), patch_size=(8, 8), emb_dim=1024, mlp_dim=2048, num_heads=16, num_layers=24,
                  n_classes=num_classes, dropout_rate=0.35, at_d_r=0.25).to(device)
     # FER_VT = MyViT((1, 48, 48), n_patches=8, n_blocks=4, hidden_d=1280, n_heads=16, out_d=num_classes).to(device)
     # FER_VT = resnet18_at(num_classes=7, at_type=0).to(device)
@@ -50,12 +50,12 @@ if __name__ == "__main__":
     # print("LR: ", lr_finder)
 
     # lr_scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, 0.001, 0.01)
-    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, 200, T_mult=1, eta_min=0.0001,
-                                                                        last_epoch=- 1, verbose=True)
+    # lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, 200, T_mult=1, eta_min=0.0001,
+    #                                                                     last_epoch=- 1, verbose=True)
     # FER_VT.load_state_dict(torch.load('./model/best.pth'))
     reduce_on_plateau = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', patience=15, verbose=True)
     # optimizer = Lookahead( optimizer, alpha= 0.6 , k = 10)
-    # lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=100, gamma=0.2)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=100, gamma=0.2)
     trainer = Trainer(logdir="./logs", csv_log_dir="./csv_log", model_checkpoint_dir="./model")
     model, optimizer, train_loss, valid_loss = trainer.training(FER_VT, train_loader, val_loader, criterion, optimizer,
                                                                 reduce_on_plateau, lr_scheduler, device, num_epochs)
