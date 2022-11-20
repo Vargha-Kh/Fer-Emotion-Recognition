c7a0e0571d4ba1b746e6361227f1f8778ea1b189
Vargha Khalokhi <vkhalokhi@gmail.com>
2022-11-20 18:55:44 +0330

    model with VIT

--- Pytorch/dataloader.py
+++ Pytorch/dataloader.py
@@ -9,8 +9,8 @@ def get_dataset(directory="./fer2013", batch_size=128, img_size=48):
     transform_train = transforms.Compose(
         [transforms.Resize((img_size, img_size)),
          transforms.Grayscale(num_output_channels=3),
-         transforms.RandomRotation(0.3),
-         transforms.RandomHorizontalFlip(),
+         # transforms.RandomRotation(0.3),
+         # transforms.RandomHorizontalFlip(),
          transforms.ToTensor(),
          transforms.Normalize(mean, std)])
 
