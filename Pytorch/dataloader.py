e598566d80dd1de2692a118449b60fbf453e58ad
Vargha Khalokhi <vkhalokhi@gmail.com>
2022-11-24 02:58:13 +0330

    model with VIT

--- Pytorch/dataloader.py
+++ Pytorch/dataloader.py
@@ -1,16 +1,31 @@
 import torch
 from torchvision import transforms, datasets
 from torch.utils.data import DataLoader
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
 
 def get_dataset(directory="./fer2013", batch_size=128, img_size=48):
     mean = [0.485, 0.456, 0.406]
     std = [0.229, 0.224, 0.225]
+
+    torchvision_transform = transforms.Compose([
+        A.Resize((img_size, img_size)),
+        A.toRGB(),
+        A.RandomCrop(8),
+        A.HorizontalFlip(),
+        A.RandomBrightnessContrast(p=0.2),
+        A.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225],
+        ), ToTensorV2()
+    ])
+
     transform_train = transforms.Compose(
         [transforms.Resize((img_size, img_size)),
          transforms.Grayscale(num_output_channels=3),
-         # transforms.RandomRotation(0.3),
-         # transforms.RandomHorizontalFlip(),
+         transforms.RandomRotation(0.3),
+         transforms.RandomHorizontalFlip(),
          transforms.ToTensor(),
          transforms.Normalize(mean, std)])
 
@@ -22,8 +37,8 @@ def get_dataset(directory="./fer2013", batch_size=128, img_size=48):
          transforms.ToTensor(),
          transforms.Normalize(mean, std)])
 
-    train_data = datasets.ImageFolder(directory + '/train', transform=transform_train)
-    val_data = datasets.ImageFolder(directory + '/val', transform=transform_val)
+    train_data = datasets.ImageFolder(directory + '/train', transform=torchvision_transform)
+    val_data = datasets.ImageFolder(directory + '/val', transform=torchvision_transform)
 
     train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
     val_loader = torch.utils.data.DataLoader(val_data, batch_size=batch_size)
