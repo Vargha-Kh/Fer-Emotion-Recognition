d1aa691f19462e8d69c082c025b5a6eb1e29db8f
Vargha Khalokhi <vkhalokhi@gmail.com>
2022-11-24 03:21:36 +0330

    model with VIT

--- Pytorch/dataloader.py
+++ Pytorch/dataloader.py
@@ -3,24 +3,37 @@ from torchvision import transforms, datasets
 from torch.utils.data import DataLoader
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
-# from albumentations.augmentations.transforms import ToRGB
+import imgaug.augmenters as iaa
+
+
+def augmentation(data_dir):
+    augmented = list()
+    s = iaa.Sequential([
+        iaa.PiecewiseAffine(scale=0.05),
+        iaa.Rotate((-30, 30)),
+        iaa.TranslateX(px=(-30, 30)),
+        iaa.TranslateY(px=(-30, 30)),
+        iaa.Grayscale(0.6),
+        iaa.Fliplr(1.0),
+        iaa.Crop(percent=0.02),
+        iaa.ChangeColorTemperature((6000, 11000))
+    ])
+    train_transforms = transforms.Compose([transforms.Resize((48, 48))])
+    images = datasets.ImageFolder(data_dir, transform=train_transforms)
+    clls = datasets.ImageFolder(data_dir).class_to_idx
+    for img in images:
+        img_ = list(img)
+        for name, indx in clls.items():
+            if s in aug_dict[name]:
+                tf = transforms.Compose([s.augment_image, transforms.ToTensor()])
+                tf(np.array(img_[0]))
+    augmented.append(images)
+    return augmented
 
 
 def get_dataset(directory="./fer2013", batch_size=128, img_size=48):
     mean = [0.485, 0.456, 0.406]
     std = [0.229, 0.224, 0.225]
-
-    torchvision_transform = transforms.Compose([
-        A.Resize(img_size, img_size),
-        # ToRGB(),
-        A.RandomCrop(8,8),
-        A.HorizontalFlip(),
-        A.RandomBrightnessContrast(p=0.2),
-        A.Normalize(
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225],
-        ), ToTensorV2()
-    ])
 
     transform_train = transforms.Compose(
         [transforms.Resize((img_size, img_size)),
@@ -38,8 +51,12 @@ def get_dataset(directory="./fer2013", batch_size=128, img_size=48):
          transforms.ToTensor(),
          transforms.Normalize(mean, std)])
 
-    train_data = datasets.ImageFolder(directory + '/train', transform=torchvision_transform)
-    val_data = datasets.ImageFolder(directory + '/val', transform=torchvision_transform)
+    train_data = datasets.ImageFolder(directory + '/train', transform=transform_train)
+    val_data = datasets.ImageFolder(directory + '/val', transform=transform_val)
+
+    augmented_data = augmentation(directory + '/train')
+    augmented_data.append(train_data)
+    train_data = torch.utils.data.ConcatDataset(augmented_data)
 
     train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
     val_loader = torch.utils.data.DataLoader(val_data, batch_size=batch_size)
