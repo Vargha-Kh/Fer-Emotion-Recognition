7668a951a65aeea7b8bc338425859c7c5db26271
Vargha Khalokhi <vkhalokhi@gmail.com>
2022-11-28 01:01:52 +0330

    fer2013 classifier final

--- Pytorch/FERVT.py
+++ Pytorch/FERVT.py
@@ -189,8 +189,8 @@ class VTA(nn.Module):
     def __init__(self):
         super(VTA, self).__init__()
 
-        self.transformer = Transformer(num_layers=10, dim=192, num_heads=4,
-                                       ff_dim=768, dropout=0.25)
+        self.transformer = Transformer(num_layers=12, dim=192, num_heads=8,
+                                       ff_dim=768, dropout=0.1)
         self.layernorm = nn.LayerNorm(192)
         self.fc = nn.Linear(192, 7)
 
